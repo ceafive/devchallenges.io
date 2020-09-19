411552399d05289bccefd6548a8174dfe71781c9
ceafive <ceafive@gmail.com>
2020-09-19 22:16:10 +0000

    configure purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,33 @@
+const tailwindcss = require("tailwindcss")("./src/tailwind.config.js")
+const cssnano = require("cssnano")
+const purgecss = require("@fullhuman/postcss-purgecss")
+const autoprefixer = require("autoprefixer")
+
 module.exports = {
-  plugins: [require("tailwindcss")("./src/tailwind.config.js")],
+  plugins: [
+    autoprefixer,
+    tailwindcss,
+    cssnano({
+      preset: [
+        "default",
+        {
+          discardComments: {
+            removeAll: true,
+          },
+        },
+      ],
+    }),
+    process.env.NODE_ENV === "production" &&
+      purgecss({
+        content: ["./src/**/*.js", "./src/**/*.jsx", "./public/**/*.html"],
+        // defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        // // content =>
+        // //   content.match(/[A-Za-z0-9-_:\/]+/g) || []
+        // whitelistPatterns: [
+        //   /-(leave|enter|appear)(|-(to|from|active))$/,
+        //   /^(?!(|.*?:)cursor-move).+-move$/,
+        //   /^router-link(|-exact)-active$/,
+        // ],
+      }),
+  ],
 }
